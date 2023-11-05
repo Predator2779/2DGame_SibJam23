dd9b2bad4d789e2c84c5845fd5ab1d20f5b5511c
Vsevolod Merenkov <loredan725@gmail.com>
2023-11-05 16:20:18 +0300

    simple melee ai

---
+++ Assets/Scripts/Character/AI/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Trigger : MonoBehaviour
+{
+    public TriggerEnterCallback _triggerEnterCallback;
+    public TriggerExitCallback _triggerExitCallback;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        _triggerEnterCallback?.Invoke(transform, col);
+    }
+    
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        _triggerExitCallback?.Invoke(transform, col);
+    }
+
+    public delegate void TriggerEnterCallback(Transform trigger, Collider2D collider);
+    public delegate void TriggerExitCallback(Transform trigger, Collider2D collider);
+}
