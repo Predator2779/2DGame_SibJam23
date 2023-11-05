5b2b0044077a2f7b828621c1a7cfeeef16ebf25e
Vsevolod Merenkov <loredan725@gmail.com>
2023-11-06 01:12:07 +0300

    Added archer and boss range capability

---
+++ Assets/Scripts/Character/Classes/Archer.cs
@@ -0,0 +1,18 @@
+﻿using Character.Item;
+using UnityEngine;
+
+namespace Scripts.Character.Classes
+{
+    public class Archer : Person
+    {
+        [SerializeField] private Arrow arrowPrefab;
+
+        public void Shoot()
+        {
+            var rotation = transform.rotation;
+            var arrow = Instantiate(arrowPrefab, transform.position + Vector3.up * 2 + rotation * Vector3.right * 7,
+                rotation * Quaternion.Euler(0, 180, 0));
+            arrow.GetComponent<Rigidbody2D>().velocity = rotation * Vector2.right * arrow.speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Character/Classes/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Character.Classes
+{
+    public class Player : Warrior
+    {
+        [SerializeField] private int _weaponIndex;
+        [SerializeField] private List<Weapon> _weapons = new();
+
+        public Weapon ChangeWeapon()
+        {
+            if (_weapons.Count > 0)
+            {
+                int nextIndex = _weaponIndex + 1;
+            
+                if (nextIndex >= _weapons.Count) nextIndex = 0;
+                
+                currentWeapon = _weapons[nextIndex];
+                _weaponIndex = nextIndex;
+
+                return currentWeapon;
+            }
+
+            return null;
+        }
+        
+        public void AddWeapon(Weapon weapon)
+        {
+            if (!_weapons.Contains(weapon))
+            {
+                _weapons.Add(weapon);
+                weapon.GetComponentInChildren<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+}
