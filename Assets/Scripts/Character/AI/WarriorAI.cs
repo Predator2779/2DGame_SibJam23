1087410aae2a0c757b424f90dcc9813d115ea17c
Vsevolod Merenkov <loredan725@gmail.com>
2023-11-06 23:05:36 +0300

    Added warrior turn

--- Assets/Scripts/Character/AI/WarriorAI.cs
+++ Assets/Scripts/Character/AI/WarriorAI.cs
@@ -7,30 +7,39 @@ public class WarriorAI : CharacterAI
     private Transform _enemy;
     protected Warrior _warrior;
 
-    protected override void Awake() => _warrior = GetComponent<Warrior>();
+    private bool _isInMeleeRange;
+
+    protected override void Awake()
+    {
+        _enemy = GameObject.FindWithTag("Player").transform;
+        _warrior = GetComponent<Warrior>();
+    }
 
     protected override void Update()
     {
         base.Update();
+        
+        _warrior.RotateByAngle(_warrior.transform,
+            _enemy.transform.position.x > transform.position.x ? 0f : 180f);
 
         CheckEnemy();
     }
 
     private void CheckEnemy()
     {
-        if (_enemy != null && _enemy.TryGetComponent(out HealthProcessor _))
+        if (_isInMeleeRange && _enemy.TryGetComponent(out HealthProcessor _))
             _warrior.Attack();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-            _enemy = collision.transform;
+            _isInMeleeRange = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-            _enemy = null;
+            _isInMeleeRange = false;
     }
 }
