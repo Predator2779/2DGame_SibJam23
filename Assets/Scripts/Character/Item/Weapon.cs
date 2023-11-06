b37b6553cf6bbcd7827e8908538bd676582984df
Predator2779 <predator2779@gmail.com>
2023-11-07 00:24:48 +0500

    fixed strikes

--- Assets/Scripts/Character/Item/Weapon.cs
+++ Assets/Scripts/Character/Item/Weapon.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Weapon : MonoBehaviour
 {
@@ -9,7 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] protected bool _destroyAfterUsing;
     [SerializeField] private float _delayUsing = 0.1f;
 
-    [FormerlySerializedAs("_undamagedWeapon")] public bool undamagedWeapon;
+    public bool undamagedWeapon;
     public int Damage => _damage * _damageFactor;
     private int _damageFactor = 1;
     private bool _canUse = true;
